3b587ad93450309a547fc084247b352e5cb36a7f
a.dadle <alexandr.dadle@gmail.com>
2022-11-25 18:07:48 +0100

    Probujem poluczit otwet

--- modules/telegramBot/controllers/BotController.php
+++ modules/telegramBot/controllers/BotController.php
@@ -3,10 +3,12 @@
 namespace app\modules\telegramBot\controllers;
 
 use app\modules\telegramBot\TelegramModule;
+use dicr\telegram\entity\Message;
 use dicr\telegram\entity\Update;
 use dicr\telegram\entity\WebhookInfo;
 use dicr\telegram\request\DeleteWebHook;
 use dicr\telegram\request\GetWebhookInfo;
+use dicr\telegram\request\SendMessage;
 use dicr\telegram\TelegramResponse;
 use Yii;
 use yii\web\Controller;
@@ -37,6 +39,13 @@ class BotController extends Controller
         $response = Yii::$app->request->post();
 
         Yii::error($response);
+
+        $request = $this->telegramModule->createRequest([
+            'class' => SendMessage::class,
+            'chatId' => '347236018',
+            'text' => $response['text'],
+        ]);
+        $request->send();
 
 
         return true;
