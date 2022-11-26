017c0053cbaa1c0b6868db17ab350b763b220988
a.dadle <alexandr.dadle@gmail.com>
2022-11-26 20:18:48 +0100

    123123

--- components/telegramRequests/TelegramRequest.php
+++ components/telegramRequests/TelegramRequest.php
@@ -41,15 +41,15 @@ abstract class TelegramRequest extends \dicr\telegram\TelegramRequest
      */
     public function send()
     {
-        // фильтруем данные
-        $data = array_filter(
-            $this->json,
-            static fn($val): bool => $val !== null && $val !== '' && $val !== []
-        );
+//        // фильтруем данные
+//        $data = array_filter(
+//            $this->json,
+//            static fn($val): bool => $val !== null && $val !== '' && $val !== []
+//        );
 
         // создаем запрос
         $req = $this->module->httpClient()
-            ->post($this->func(), $data, [
+            ->post($this->func(), $this->json, [
                 'Content-Type' => 'application/json',
                 'Accept' => 'application/json'
             ]);
